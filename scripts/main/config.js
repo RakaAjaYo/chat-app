a39adb262558c3d0be0609daa418f4d9da12050e
RakaAjaYo <111719641+RakaAjaYo@users.noreply.github.com>
2024-11-07 21:39:39 +0700

    Update config.js

--- scripts/main/config.js
+++ scripts/main/config.js
@@ -1,12 +1,13 @@
 // ISI DENGAN PROJECT FIREBASE MILIK TEMEN-TEMEN SENDIRI
 const firebaseConfig = {
-    apiKey: "PUNYA-KAMU",
-    authDomain: "PUNYA-KAMU",
-    databaseURL: "PUNYA-KAMU",
-    projectId: "PUNYA-KAMU",
-    storageBucket: "PUNYA-KAMU",
-    messagingSenderId: "PUNYA-KAMU",
-    appId: "PUNYA-KAMU"
+  apiKey: "AIzaSyClyZlGq2uPc654QLyymVh2CTwcSjWC1Bo",
+  authDomain: "chat-app--world-chat.firebaseapp.com",
+  databaseURL: "https://chat-app--world-chat-default-rtdb.firebaseio.com",
+  projectId: "chat-app--world-chat",
+  storageBucket: "chat-app--world-chat.appspot.com",
+  messagingSenderId: "872704705411",
+  appId: "1:872704705411:web:3e2144383a15308edd2cb6",
+  measurementId: "G-C6LNBE4TG9"
 };
 
 // BIAR GA KELAMAAN PAS MAU PANGGIL NANTI
